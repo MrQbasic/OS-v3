ecd742de326c86e6f9bee104e74d672a520aaf3d
MrQbasic <77384846+MrQbasic@users.noreply.github.com>
2023-02-17 22:32:38 +0100

    ahci features

--- kernel/src/code/disk/ahci.c
+++ kernel/src/code/disk/ahci.c
@@ -4,31 +4,81 @@
 #include "screen.h"
 
 uint64_t* ahci_abar_phy;
-uint64_t* ahci_abar_lin;
+AHCI_HBA** ahci_abar;
 
 int ahci_init_disk(uint16_t disk_count, uint16_t* disk_addr){
     //setup abar ports
     ahci_abar_phy = (uint64_t*) mem_alloc(sizeof(uint64_t) * disk_count, 0);
-    ahci_abar_lin = (uint64_t*) mem_alloc(sizeof(uint64_t) * disk_count, 0);
+    ahci_abar = (AHCI_HBA**) mem_alloc(sizeof(AHCI_HBA*) * disk_count, 0);
     for(int i=0; i<disk_count; i++){
         uint8_t  bus = (uint8_t) (disk_addr[i] >> 0);
         uint8_t slot = (uint8_t) (disk_addr[i] >> 8);
         ahci_abar_phy[i]  = 0x000000000000FFFF & (pciConfigReadWord(bus, slot, 0, 0x24) <<  0);
-        ahci_abar_phy[i]  = 0x00000000FFFF0000 & (pciConfigReadWord(bus, slot, 0, 0x26) << 16);
-        ahci_abar_lin[i]  = mem_alloc(0x1000, 0x1000);
-
+        ahci_abar_phy[i] |= 0x00000000FFFF0000 & (pciConfigReadWord(bus, slot, 0, 0x26) << 16);
+        ahci_abar[i] = (AHCI_HBA*) mem_alloc(0x1000, 0x1000);
+        //remap memory so match abar
         int error = 0;
         uint64_t paddr;
-        error |= page_trace_PML4(ahci_abar_lin[i], &paddr);
+        error |= page_trace_PML4((uint64_t) ahci_abar[i], &paddr);
         error |= mem_pfree(paddr,1);
         //unexpected error 
         if(error != 0){
             screenPrint("OUT OF MEMORY!/n/e");
             while(1);
         }
-        page_map_PML4(ahci_abar_lin[i], ahci_abar_phy[i], 0b00010001, 0x00);
+        page_map_PML4((uint64_t) ahci_abar[i], ahci_abar_phy[i], 0b00010001, 0x00);
+        //print implemented ports / devices
+        uint32_t impl = ahci_abar[i]->ports_implemented;
+        int port_index = 0;
+        while(impl != 0){
+            int type = ahci_port_check_type(&(ahci_abar[i]->ports[port_index]));
+            if(type == AHCI_DEV_PM)     screenPrint("PM /e");
+            if(type == AHCI_DEV_SATA)   screenPrint("SATA /e");
+            if(type == AHCI_DEV_SATAPI) screenPrint("SATAPI /e");
+            if(type == AHCI_DEV_SEMB)   screenPrint("SEMB /e");
+            if(type != AHCI_DEV_NULL){
+                screenPrint("DEV @ PORT: /xB LIN: /xQ PHY: /xQ/n/e",(uint8_t)port_index, ahci_abar[i],ahci_abar_phy[i]);
+            }
+            port_index++;
+            impl = impl >> 1;
+        }
+        //rebase ports
+
+
+
     }
     //setup 
 
     return 0;
+}
+
+int ahci_port_check_type(AHCI_HBA_PORT* port){
+    uint32_t status = port->sata_status;
+    uint8_t ipm = (status >> 8) & 0x0F;
+    uint8_t det = status & 0x0F;
+    //check if a drive is usable
+    if(det != AHCI_HBA_PORT_DET_PRESENT) return AHCI_DEV_NULL;
+    if(ipm != AHCI_HBA_PORT_IPM_ACTIVE ) return AHCI_DEV_NULL;
+    //check type
+    uint32_t signature = port->signature;
+    if(signature == SATA_SIG_ATAPI) return AHCI_DEV_SATAPI;
+    if(signature == SATA_SIG_SEMB)  return AHCI_DEV_SEMB;
+    if(signature == SATA_SIG_PM)    return AHCI_DEV_PM;
+    return AHCI_DEV_SATA;
+}
+
+void ahci_port_stop_cmd(AHCI_HBA_PORT* port){
+    port->cmd &= ~AHCI_HBA_PORT_CMD_ST;
+    port->cmd &= ~AHCI_HBA_PORT_CMD_FRE;
+    while(1){
+        if(port->cmd & AHCI_HBA_PORT_CMD_FR) continue;
+        if(port->cmd & AHCI_HBA_PORT_CMD_CR) continue;
+        break;
+    }
+}
+
+void ahci_port_start_cmd(AHCI_HBA_PORT* port){
+    while(port->cmd & AHCI_HBA_PORT_CMD_CR);
+    port->cmd |= AHCI_HBA_PORT_CMD_FRE;
+    port->cmd |= AHCI_HBA_PORT_CMD_ST;
 }
--- kernel/src/include/disk/ahci.h
+++ kernel/src/include/disk/ahci.h
@@ -1,5 +1,25 @@
 #include "def.h"
 
+#define	SATA_SIG_ATA	0x00000101
+#define	SATA_SIG_ATAPI	0xEB140101
+#define	SATA_SIG_SEMB	0xC33C0101
+#define	SATA_SIG_PM  	0x96690101
+ 
+#define AHCI_DEV_NULL 0
+#define AHCI_DEV_SATA 1
+#define AHCI_DEV_SEMB 2
+#define AHCI_DEV_PM 3
+#define AHCI_DEV_SATAPI 4
+ 
+#define AHCI_HBA_PORT_IPM_ACTIVE 1
+#define AHCI_HBA_PORT_DET_PRESENT 3
+
+#define AHCI_HBA_PORT_CMD_ST  0x0001
+#define AHCI_HBA_PORT_CMD_FRE 0x0010
+#define AHCI_HBA_PORT_CMD_FR  0x4000
+#define AHCI_HBA_PORT_CMD_CR  0x8000
+ 
+
 typedef volatile struct AHCI_HBA_PORT{
     uint32_t clb;
     uint32_t clbu;
@@ -10,13 +30,13 @@ typedef volatile struct AHCI_HBA_PORT{
     uint32_t cmd; 
     uint32_t zero_0;
     uint32_t tfd;
-    uint32_t sig;
-    uint32_t ssts;
-    uint32_t sctl;
-    uint32_t serr;
-    uint32_t sact;
+    uint32_t signature;
+    uint32_t sata_status;
+    uint32_t sata_control;
+    uint32_t sata_error;
+    uint32_t sata_active;
     uint32_t ci;
-    uint32_t sntf;
+    uint32_t sata_notification;
     uint32_t fbs;
     uint32_t zero_1[11];
     uint32_t vendor[4];
@@ -40,7 +60,7 @@ typedef volatile struct AHCI_HBA{
     //vendor
     uint8_t vendor[96];
     //ports
-    AHCI_HBA_PORT ports[0];
+    AHCI_HBA_PORT ports[32];
 
 } AHCI_HBA;
 
@@ -51,3 +71,8 @@ typedef volatile struct AHCI_HBA{
 int ahci_init_disk(uint16_t disk_count, uint16_t* disk_addr);
 
 int ahci_port_check_type(AHCI_HBA_PORT* port);
+
+void ahci_port_stop_cmd(AHCI_HBA_PORT* port);
+
+void ahci_port_start_cmd(AHCI_HBA_PORT* port);
+
