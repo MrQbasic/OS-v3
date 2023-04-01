329d8d3e8b327c4f5235004c7aa4267e1b0fa88c
MrQbasic <77384846+MrQbasic@users.noreply.github.com>
2023-04-01 16:32:44 +0200

    ahci in progess

--- kernel/src/code/disk/ahci.c
+++ kernel/src/code/disk/ahci.c
@@ -5,6 +5,7 @@
 
 uint64_t* ahci_abar_phy;
 AHCI_HBA** ahci_abar;
+AHCI_LIN_ADDRESS_LIST_ENTRY* lin_address_list = 0;
 
 int ahci_init_disk(uint16_t disk_count, uint16_t* disk_addr){
     //setup abar ports
@@ -44,6 +45,7 @@ int ahci_init_disk(uint16_t disk_count, uint16_t* disk_addr){
         }
         //rebase ports
         ahci_port_rebase(ahci_abar[i]);
+        ahci_port_read(ahci_abar[i]->ports,0,1,0);
     }
     //setup 
 
@@ -81,7 +83,20 @@ void ahci_port_start_cmd(AHCI_HBA_PORT* port){
     port->cmd |= AHCI_HBA_PORT_CMD_ST;
 }
 
+void ahci_append_to_lin_address_list(AHCI_LIN_ADDRESS_LIST_ENTRY* entry){
+    AHCI_LIN_ADDRESS_LIST_ENTRY* current_entry = lin_address_list;
+    if(current_entry == 0){
+        lin_address_list = entry;
+        return;
+    }
+    while(current_entry->next != 0){
+        current_entry = (AHCI_LIN_ADDRESS_LIST_ENTRY*) current_entry->next;
+    }
+    current_entry->next = (uint64_t) entry;
+}
+
 void ahci_port_rebase(AHCI_HBA* ahci_hba){
+    //setup going over all ports
     uint64_t impl = (uint64_t) ahci_hba->ports_implemented;
     impl = impl << 1;
     int port_index = -1;
@@ -94,24 +109,172 @@ void ahci_port_rebase(AHCI_HBA* ahci_hba){
         //get port ready
         AHCI_HBA_PORT* port = &(ahci_hba->ports[port_index]);
         ahci_port_stop_cmd(port);
+        //setup address list entry
+        AHCI_LIN_ADDRESS_LIST_ENTRY* addr_list = 
+                    (AHCI_LIN_ADDRESS_LIST_ENTRY*) mem_alloc(sizeof(AHCI_LIN_ADDRESS_LIST_ENTRY),0);
+        addr_list->next = 0;
+        addr_list->id = (uint64_t) port;
+        ahci_append_to_lin_address_list(addr_list);
         //remap command list
         uint64_t cmd_list_virt = mem_alloc(0x400,0x400);
         uint64_t cmd_list_phy;
         page_trace_PML4(cmd_list_virt, &cmd_list_phy);
         cmd_list_phy += cmd_list_virt & 0x0FFF;
+        //point hardware to new space
         port->clb  = (uint32_t)  cmd_list_phy;
         port->clbu = (uint32_t) (cmd_list_phy >> 32);
+        //clear allocated memory
         mem_write((void*) cmd_list_virt, 0,1024);
+        //add to phy address list entry
+        addr_list->cmd_list = cmd_list_virt; 
         //remap fis
         uint64_t fis_virt = mem_alloc(0x100, 0x100);
         uint64_t fis_phy;
         page_trace_PML4(fis_virt, &fis_phy);
         fis_phy += cmd_list_virt & 0x0FFF;
+        //point hardware to new space
         port->fb  = (uint32_t)  cmd_list_phy;
         port->fbu = (uint32_t) (cmd_list_phy >> 32);
+        //clear allocated memory
         mem_write((void*) fis_virt, 0, 256);
-
-
-        //screenPrint("ASDF/n/e");
+        //add to phy address list entry
+        addr_list->fis = fis_virt;
+        //remap command 
+        AHCI_HBA_CMD_HEADER *cmdheader = (AHCI_HBA_CMD_HEADER*) cmd_list_virt;
+        for(int i=0; i<32; i++){
+            cmdheader[i].phy_region_descriptor_table_length = 8;
+            //remap cmd tbl
+            uint64_t virt_addr = mem_alloc(0x100, 0x100); 
+            uint64_t phy_addr;
+            page_trace_PML4(virt_addr, &phy_addr);
+            //point hardware to new space
+            cmdheader[i].cmd_tbl_base_lower = (uint32_t) (phy_addr);
+            cmdheader[i].cmd_tbl_base_upper = (uint32_t) (phy_addr >> 32);
+            //clear allocated memory
+            uint64_t* tmp = (uint64_t*) virt_addr;
+            for(int i=0; i<(256/8); i++){
+                *tmp = 0;
+            }
+            //add to phy address list
+            addr_list->cmd_tbl[i] = virt_addr;
+        }
+        //restart port
+        ahci_port_start_cmd(port);
     }
+}
+
+#define ATA_DEV_BUSY 0x80
+#define ATA_DEV_DRQ 0x08
+
+ 
+// Find a free command list slot
+int find_cmdslot(AHCI_HBA_PORT *port){
+	// If not set in SACT and CI, the slot is free
+	uint32_t slots = (port->sata_active | port->ci);
+    screenPrint("/xD/n/e",slots);
+	for (int i=0; i<1; i++){
+        
+		if ((slots&1) == 0) return i;
+		slots >>= 1;
+	}
+	screenPrint("Cannot find free command list entry/n/e");
+	return -1;
+}
+
+#define HBA_PxIS_TFES (1 << 30) 
+#define ATA_CMD_READ_DMA_EX 0x25
+
+//1 is bad
+int ahci_port_read(AHCI_HBA_PORT *port, uint64_t start, uint32_t count, uint64_t buf){
+    screenPrint("/0---AHCI-READ---/n/e");
+    //search address list
+    if(lin_address_list == 0) return 1;
+    AHCI_LIN_ADDRESS_LIST_ENTRY* addr_list = lin_address_list;
+    while(addr_list->id != (uint64_t) port){
+        if(addr_list->next == 0) return 1;
+        addr_list = (AHCI_LIN_ADDRESS_LIST_ENTRY*) addr_list->next;
+    }
+    screenPrint("PORT ADDR    : /xQ/n/e",(uint64_t) port);
+    screenPrint("PORT ADDR_TBL: /xQ/n/e",(uint64_t) addr_list);
+    //find cmd slot
+    port->is = (uint32_t) -1;		// Clear pending interrupt bits
+	int spin = 0; // Spin lock timeout counter
+	int slot = find_cmdslot(port);
+	if (slot == -1) return 1;
+    
+	AHCI_HBA_CMD_HEADER *cmdheader = (AHCI_HBA_CMD_HEADER*) addr_list->cmd_list;
+
+	cmdheader += slot;
+	cmdheader->cmd_FIS_length = sizeof(FIS_REG_H2D)/sizeof(uint32_t);	// Command FIS size
+	cmdheader->w = 0;		// Read from device
+	cmdheader->phy_region_descriptor_table_length = (uint16_t)((count-1)>>4) + 1;	// PRDT entries count
+ 
+	AHCI_HBA_CMD_TABLE *cmdtbl;
+    cmdtbl = (AHCI_HBA_CMD_TABLE*)((uint64_t) cmdheader->cmd_tbl_base_lower);
+    cmdtbl = (AHCI_HBA_CMD_TABLE*)((uint64_t) cmdheader->cmd_tbl_base_upper << 32);
+    
+    uint64_t size = sizeof(AHCI_HBA_CMD_TABLE) + (cmdheader->phy_region_descriptor_table_length-1)*sizeof(HBA_PRDT_ENTRY);
+    uint8_t *tmp = (uint8_t*) cmdtbl;
+    for(int i=0; i<size; i++){
+       *tmp = 0;
+    }
+
+	// 8K bytes (16 sectors) per PRDT
+    int i=0;
+	for (i=0; i<cmdheader->phy_region_descriptor_table_length-1; i++)
+	{
+		cmdtbl->prdt_entry[i].data_base_lower = (uint32_t) (buf);
+        cmdtbl->prdt_entry[i].data_base_upper = (uint32_t) (buf >> 32);
+		cmdtbl->prdt_entry[i].data_byte_count = 8*1024-1;	// 8K bytes (this value should always be set to 1 less than the actual value)
+		cmdtbl->prdt_entry[i].interrupt = 1;
+		buf += 4*1024;	// 4K words
+		count -= 16;	// 16 sectors
+	}
+	// Last entry
+	cmdtbl->prdt_entry[i].data_base_lower = (uint32_t) (buf);
+    cmdtbl->prdt_entry[i].data_base_upper = (uint32_t) (buf >> 32);
+	cmdtbl->prdt_entry[i].data_byte_count = (count<<9)-1;	// 512 bytes per sector
+	cmdtbl->prdt_entry[i].interrupt = 1;
+ 
+	// Setup command
+	FIS_REG_H2D *cmdfis = (FIS_REG_H2D*)(&cmdtbl->cmd_FIS);
+ 
+	cmdfis->fis_type = FIS_TYPE_REG_H2D;
+	cmdfis->c = 1;	// Command
+	cmdfis->command = ATA_CMD_READ_DMA_EX;
+ 
+	cmdfis->lba0 = (uint8_t)start;
+	cmdfis->lba1 = (uint8_t)(start >> 8);
+	cmdfis->lba2 = (uint8_t)(start >> 16);
+	cmdfis->lba3 = (uint8_t)(start >> 24);
+	cmdfis->lba4 = (uint8_t)(start >> 32);
+	cmdfis->lba5 = (uint8_t)(start >> 40);
+    
+    cmdfis->device = 1<<6;	// LBA mode
+ 
+	cmdfis->countl = count & 0xFF;
+	cmdfis->counth = (count >> 8) & 0xFF;
+
+	while ((port->tfd & (ATA_DEV_BUSY | ATA_DEV_DRQ)) && spin < 1000000){
+		spin++;
+	}
+	if (spin == 1000000){
+		screenPrint("Port is hung/n/e");
+		return 1;
+	}
+    
+	port->ci = 1<<slot;
+	while (1){
+		if ((port->ci & (1<<slot)) == 0) break;
+		if (port->is & HBA_PxIS_TFES){
+			screenPrint("Read disk error/n/e");
+			return 1;
+		}
+	}
+	if (port->is & HBA_PxIS_TFES){
+		screenPrint("Read disk error/n/e");
+		return 1;
+	}
+    screenPrint("/nREAD GOOD!/e");
+	return 0;
 }
--- kernel/src/include/disk/ahci.h
+++ kernel/src/include/disk/ahci.h
@@ -1,4 +1,5 @@
 #include "def.h"
+#include "ahci_fis.h"
 
 #define	SATA_SIG_ATA	0x00000101
 #define	SATA_SIG_ATAPI	0xEB140101
@@ -18,7 +19,40 @@
 #define AHCI_HBA_PORT_CMD_FRE 0x0010
 #define AHCI_HBA_PORT_CMD_FR  0x4000
 #define AHCI_HBA_PORT_CMD_CR  0x8000
- 
+
+typedef struct HBA_PRDT_ENTRY
+{
+	uint32_t data_base_lower;
+	uint32_t data_base_upper;
+	uint32_t zero_0;
+	uint32_t data_byte_count:22;
+	uint32_t zero_1:9;
+	uint32_t interrupt:1;
+} HBA_PRDT_ENTRY;
+
+typedef struct AHCI_HBA_CMD_TABLE{
+	uint8_t  cmd_FIS[64];
+	uint8_t  atapi_cmd[16];
+	uint8_t  reserved[48];
+	HBA_PRDT_ENTRY	prdt_entry[1];	// Physical region descriptor table entries, 0 ~ 65535
+} AHCI_HBA_CMD_TABLE;
+
+typedef struct AHCI_HBA_CMD_HEADER{
+	uint8_t  cmd_FIS_length:5;		// Command FIS length in DWORDS, 2 ~ 16
+	uint8_t  a:1;		// ATAPI
+	uint8_t  w:1;		// Write, 1: H2D, 0: D2H
+	uint8_t  p:1;		// Prefetchable
+	uint8_t  r:1;		// Reset
+	uint8_t  b:1;		// BIST
+	uint8_t  c:1;		// Clear busy upon R_OK
+	uint8_t  rsv0:1;	// Reserved
+	uint8_t  pmp:4;		// Port multiplier port
+	uint16_t phy_region_descriptor_table_length;		//in entries
+    volatile uint32_t phy_region_descriptor_byte_cnt;
+	uint32_t cmd_tbl_base_lower;
+	uint32_t cmd_tbl_base_upper;
+    uint32_t reserved[4];
+} AHCI_HBA_CMD_HEADER;
 
 typedef volatile struct AHCI_HBA_PORT{
     uint32_t clb;
@@ -65,12 +99,36 @@ typedef volatile struct AHCI_HBA{
 } AHCI_HBA;
 
 
+typedef struct AHCI_LIN_ADDRESS_LIST_ENTRY{
+    //make it a list
+    uint64_t next;
+    //id (port adress)
+    uint64_t id;
+    //phy base addresses
+    uint64_t cmd_list;
+    uint64_t fis;
+    uint64_t cmd_tbl[32];
+} AHCI_LIN_ADDRESS_LIST_ENTRY;
+
+
+
 //returns:
 //  0 -> OK
 //  1 -> ERROR
 int ahci_init_disk(uint16_t disk_count, uint16_t* disk_addr);
 
+//returns:
+//  0 -> ERROR
+//  1 -> SATA
+//  2 -> SEMB
+//  3 -> PM
+//  4 -> SATAPI
 int ahci_port_check_type(AHCI_HBA_PORT* port);
+
+//returns:
+//  0 -> OK
+//  1 -> ERROR
+int ahci_port_read(AHCI_HBA_PORT *port, uint64_t start, uint32_t count, uint64_t buf);
 
 void ahci_port_stop_cmd(AHCI_HBA_PORT* port);
 
