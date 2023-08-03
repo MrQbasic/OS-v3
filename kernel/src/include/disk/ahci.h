396f20f045bc0fa52acc4de57d07a90be7a856d5
MrQbasic <77384846+MrQbasic@users.noreply.github.com>
2023-08-03 15:17:47 +0200

    Update ahci.h

--- kernel/src/include/disk/ahci.h
+++ kernel/src/include/disk/ahci.h
@@ -55,25 +55,25 @@ typedef struct AHCI_HBA_CMD_HEADER{
 } AHCI_HBA_CMD_HEADER;
 
 typedef volatile struct AHCI_HBA_PORT{
-    uint32_t clb;		// 0x00, command list base address, 1K-byte aligned
-	uint32_t clbu;		// 0x04, command list base address upper 32 bits
-	uint32_t fb;		// 0x08, FIS base address, 256-byte aligned
-	uint32_t fbu;		// 0x0C, FIS base address upper 32 bits
-	uint32_t is;		// 0x10, interrupt status
-	uint32_t ie;		// 0x14, interrupt enable
-	uint32_t cmd;		// 0x18, command and status
-	uint32_t rsv0;		// 0x1C, Reserved
-	uint32_t tfd;		// 0x20, task file data
-	uint32_t sig;		// 0x24, signature
-	uint32_t ssts;		// 0x28, SATA status (SCR0:SStatus)
-	uint32_t sctl;		// 0x2C, SATA control (SCR2:SControl)
-	uint32_t serr;		// 0x30, SATA error (SCR1:SError)
-	uint32_t sact;		// 0x34, SATA active (SCR3:SActive)
-	uint32_t ci;		// 0x38, command issue
-	uint32_t sntf;		// 0x3C, SATA notification (SCR4:SNotification)
-	uint32_t fbs;		// 0x40, FIS-based switch control
-	uint32_t rsv1[11];	// 0x44 ~ 0x6F, Reserved
-	uint32_t vendor[4];	// 0x70 ~ 0x7F, vendor specific
+    uint32_t clb;					// 0x00, command list base address, 1K-byte aligned
+    uint32_t clbu;					// 0x04, command list base address upper 32 bits
+    uint32_t fb;					// 0x08, FIS base address, 256-byte aligned
+    uint32_t fbu;					// 0x0C, FIS base address upper 32 bits
+    uint32_t is;					// 0x10, interrupt status
+    uint32_t ie;					// 0x14, interrupt enable
+    uint32_t cmd; 					// 0x18, command and status
+    uint32_t zero_0;				// 0x1C, Reserved
+    uint32_t tfd;					// 0x20, task file data
+    uint32_t signature;				// 0x24, signature
+    uint32_t sata_status;			// 0x28, SATA status (SCR0:SStatus)
+    uint32_t sata_control;			// 0x2C, SATA control (SCR2:SControl)
+    uint32_t sata_error;			// 0x30, SATA error (SCR1:SError)
+    uint32_t sata_active;			// 0x34, SATA active (SCR3:SActive)
+    uint32_t ci;					// 0x38, command issue
+    uint32_t sata_notification;		// 0x3C, SATA notification (SCR4:SNotification)
+    uint32_t fbs;					// 0x40, FIS-based switch control
+    uint32_t zero_1[11];			// 0x44 ~ 0x6F, Reserved
+    uint32_t vendor[4];				// 0x70 ~ 0x7F, vendor specific
 } AHCI_HBA_PORT;
 
 typedef volatile struct AHCI_HBA{
