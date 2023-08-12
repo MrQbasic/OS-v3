d21e7d62a9cb42ce86cbe637146bc39021f84415
MrQbasic <77384846+MrQbasic@users.noreply.github.com>
2023-08-12 13:32:29 +0200

    timer fuction

--- kernel/src/code/cpu/timer-isr.s
+++ kernel/src/code/cpu/timer-isr.s
@@ -1,31 +1,17 @@
+extern pi
+
 SECTION .data
     global syscounter_cnt
-    asdf: dq 0
-    syscounter_cnt: dq 0x1234
+    syscounter_cnt: dq 0
 
 SECTION .text
     global syscounter_isr
 
 syscounter_isr:
     cli
-    push rax
-    push rbx
-    push rcx
-    push rdx
-    push rsi
-    push rdi
-
-    mov rdi, syscounter_cnt
-    inc QWORD [rdi]
-
+    push rax   
+    inc QWORD [syscounter_cnt]
     mov al, 0x20
     out 0x20, al
-    out 0xA0, al
-
-    pop rdi
-    pop rsi
-    pop rdx
-    pop rcx
-    pop rbx
     pop rax
     iretq
