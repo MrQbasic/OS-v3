e513cc650a9db7f9674bd5a6f619a56011a8c20b
MrQbasic <77384846+MrQbasic@users.noreply.github.com>
2023-08-12 14:02:48 +0200

    new int handler

--- kernel/src/code/cpu/idt-isr.s
+++ kernel/src/code/cpu/idt-isr.s
@@ -2,19 +2,16 @@ extern pic_eoi
 extern screenPrintChars
 
 SECTION .data
-    ist_isr_default_msg: db "INT! /n/e"
 
 SECTION .text
     GLOBAL idt_isr_default
 
 idt_isr_default:
-    push rax
+    mov rcx, 80*20
+    mov rbx, 0xB8000
+.fill_loop1:
+    mov BYTE [rbx+1], 0x40
+    add rbx, 2
+    loop .fill_loop1
 
-    mov rax, 0xB8000
-    inc WORD [rax]
-
-    mov al, 0x20
-    out 0x20, al
-    
-    pop rax
-    iretq
+    jmp $
